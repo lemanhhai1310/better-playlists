6511e5eb6326eaad62aeec4d176dcc6b7dded467
Hai LE Manh <hai.lemanh.1310@gmail.com>
2018-07-19 16:45:35 +0700

    hihi

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 let DefaultTextColor = '#fff';
+let fakeServerData = {
+    user: {
+        name: 'David',
+        playlists: [
+            {
+                name: 'My favorites',
+                songs: ['Beat It', 'Cannelloni Makaroni', 'Rosa helikopter']
+            },
+            {
+                name: 'Em ơi',
+                songs: ['Beat It', 'Cannelloni Makaroni', 'Rosa helikopter']
+            },
+            {
+                name: 'Anh không đòi quà',
+                songs: ['Beat It', 'Cannelloni Makaroni', 'Rosa helikopter']
+            }
+        ]
+    }
+};
 
 class Aggregate extends Component{
     render(){
         return(
             <div className="aggregate" style={{width: '40%', display: 'inline-block'}}>
-                <h2 style={{color: DefaultTextColor}}>Number text</h2>
+                <h2 style={{color: DefaultTextColor}}>{this.props.playlists && this.props.playlists.length} Text</h2>
             </div>
         );
     }
@@ -18,7 +37,7 @@ class Filter extends Component{
         return(
             <div>
                 <img src="" alt=""/>
-                <input type="text" name="" id=""/>
+                <input type="text" name="" id="" placeholder="Search..."/>
                 <button>Filter</button>
             </div>
         );
@@ -36,6 +55,7 @@ class Playlist extends Component{
                     <li>Song 2</li>
                     <li>Song 3</li>
                     <li>Song 4</li>
+
                 </ul>
             </div>
         );
@@ -44,17 +64,39 @@ class Playlist extends Component{
 
 class App extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            serverData: {
+
+            }
+        }
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({serverData: fakeServerData});
+        }, 1000);
+    }
+
     render() {
+        console.log(this.state.serverData);
         return (
-            <div className="App container">
-                <h1>Title</h1>
-                <Aggregate />
-                <Aggregate />
-                <Filter />
-                <Playlist />
-                <Playlist />
-                <Playlist />
-                <Playlist />
+            <div className="App uk-container">
+                {this.state.serverData.user ?
+                    <div>
+                        <h1>
+                            {this.state.serverData.user.name}'s Playlists
+                        </h1>
+                        <Aggregate playlists={this.state.serverData.user.playlists} />
+                        <Aggregate />
+                        <Filter />
+                        <Playlist />
+                        <Playlist />
+                        <Playlist />
+                        <Playlist />
+                    </div> : 'Loading...'
+                }
             </div>
         );
     }
